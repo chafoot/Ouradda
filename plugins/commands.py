b7590f1d3d3c6170b0751ab36aec9d710275b3e2
Arun Kumar <46559817+chafoot@users.noreply.github.com>
2024-05-09 07:35:47 +0530

    Given TelegramBot.log

--- plugins/commands.py
+++ plugins/commands.py
@@ -330,7 +330,7 @@ async def channel_info(bot, message):
 async def log_file(bot, message):
     """Send log file"""
     try:
-        await message.reply_document('OuraddaaBot.log')
+        await message.reply_document('TelegramBot.log')
     except Exception as e:
         await message.reply(str(e))
 
